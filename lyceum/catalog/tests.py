308e7ec4fd2f8d33f0f3dc55b063e9941eec186b
Андрей <andrei@duvakin.ru>
2024-02-16 21:41:41 +0500

    поправил тесты

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,25 +1,41 @@
-import random
 from http import HTTPStatus
 
 from django.test import Client, TestCase
 
+from parameterized import parameterized
+
 
 class StaticURLTests(TestCase):
-    def test_catalog_list_endpoint(self):
-        response = Client().get("/catalog/")
-        self.assertEquals(response.status_code, HTTPStatus.OK)
 
-    def test_catalog_detail_endpoint(self):
-        digit = random.randint(1, 100)
-        response = Client().get(f"/catalog/{str(digit)}/")
-        self.assertEquals(response.status_code, HTTPStatus.OK)
+    def setUp(self):
+        self.client = Client()
 
-    def test_repeat_int_endpoint(self):
-        digit = str(random.randint(1, 100))
-        response = Client().get(f"/catalog/re/{digit}/")
-        self.assertEquals(response.content.decode(), digit)
+    @parameterized.expand(
+        [
+            ("1",),
+            ("1000",),
+        ]
+    )
+    def test_catalog_endpoints(self, digit):
+        response = self.client.get(f"/catalog/{digit}/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_redigit_endpoint(self):
-        digit = str(random.randint(1, 100))
-        response = Client().get(f"/catalog/re/{digit}/")
-        self.assertEquals(response.content.decode(), digit)
+    @parameterized.expand(
+        [
+            ("1",),
+            ("1000",),
+        ]
+    )
+    def test_repeat_int_endpoint(self, digit):
+        response = self.client.get(f"/catalog/re/{digit}/")
+        self.assertEqual(response.content.decode(), digit)
+
+    @parameterized.expand(
+        [
+            ("1",),
+            ("1000",),
+        ]
+    )
+    def test_redigit_endpoint(self, digit):
+        response = self.client.get(f"/catalog/converter/{digit}/")
+        self.assertEqual(response.content.decode(), digit)
