35034d8da1c3c7f074978c77b0f0cb5dca8409b0
Андрей <andrei@duvakin.ru>
2024-02-21 17:44:29 +0500

    поправил ошибки slug и вдимость имени name и is_published

--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -3,12 +3,12 @@ import django.db.models
 
 class AbstractRootModel(django.db.models.Model):
     name = django.db.models.TextField(
-        name="Название",
+        verbose_name="Название",
         help_text="Введите название",
         max_length=150,
     )
     is_published = django.db.models.BooleanField(
-        name="Опубликовано",
+        verbose_name="Опубликовано",
         help_text="Статус публикации",
         default=True,
     )
