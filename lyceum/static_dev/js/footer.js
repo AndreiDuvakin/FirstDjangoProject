f7c22438ebf0b29bf21ea3438297dc3b3a5cf914
Андрей <andrei@duvakin.ru>
2024-03-02 16:00:54 +0500

    поправил отображение года в footer

--- lyceum/static_dev/js/footer.js
+++ lyceum/static_dev/js/footer.js
@@ -1,6 +1,11 @@
-var currentYearClient = new Date().getFullYear();
-var currentYearServer = document.getElementById('current_year').innerText;
+var currentDate = new Date();
+var currentYearDateStr = document.getElementById("current_year").innerText;
+var currentYearDate = new Date(currentYearDateStr);
+var hourDiff = Math.abs(currentDate - currentYearDate);
+var hDiff = hourDiff / 3600 / 1000;
 
-if (currentYearClient != currentYearServer) {
-    document.getElementById('current_year').innerText = currentYearClient;
+if (hDiff < 24) {
+    document.getElementById('current_year').innerText = currentDate.getFullYear();
+} else {
+    document.getElementById('current_year').innerText = currentYearDate.getFullYear();
 }
