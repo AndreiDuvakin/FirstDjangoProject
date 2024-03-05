280d88ac4f8d444a31a912f9a163d7ad7ada08df
Андрей <andrei@duvakin.ru>
2024-03-05 18:53:44 +0500

    поправил форматирование и формирование пути в downloads/views.py

--- lyceum/download/views.py
+++ lyceum/download/views.py
@@ -1,10 +1,13 @@
-import os
+from pathlib import Path
 
 from django.http import FileResponse, Http404
 
 
 def download_file(request, file_path):
-    full_file_path = f"media/{file_path}"
-    if os.path.exists(full_file_path):
+    full_file_path = Path(f"media/{file_path}")
+    if full_file_path.exists():
         return FileResponse(open(full_file_path, "rb"), as_attachment=True)
     raise Http404("Файл не найден")
+
+
+__all__ = []
